f203705695c5fad5656ee982196e4cc72b80ea88
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-21 19:18:51 +0800

    update javascript
    added variables that keep track of user and comp scores
    added score increment in playRound function

--- app.js
+++ app.js
@@ -18,35 +18,41 @@ function getComputerInput() {
             return "scissor"
         };
 };
+
 // function that get computer input everytime a game is started
 function repeatGetComputerInput() {
     compSelection = getComputerInput()
-}
+};
+
+// Variables that keep track of computer and user score
+let userScore = 0;
+let compScore = 0;
+
 
 function playRound() {
     repeatGetComputerInput();
     if (userSelection === compSelection) {
         output.innerText = "tie";
     } else if (userSelection == "rock" && compSelection == "paper") {
-        //compScore += 1;
+        compScore += 1;
         output.innerText = "You lost the round, Paper beats Rock!";
     } else if (userSelection == "rock" && compSelection == "scissor") {
-       // userScore += 1;
+        userScore += 1;
        output.innerText = "You won the round, Rock beats Scissor!";
     } else if (userSelection == "paper" && compSelection == "rock") {
-       // userScore += 1;
+        userScore += 1;
        output.innerText = "You won the round, Paper beats Rock!";
     } else if (userSelection == "paper" && compSelection == "scissor") {
-        //compScore += 1;
+        compScore += 1;
         output.innerText = "You lost the round, Scissor beats Paper!";
     } else if (userSelection == "scissor" && compSelection == "rock") {
-       // compScore += 1;
+        compScore += 1;
        output.innerText = "You lost the round, Rock beats Scissor!";
     } else {
-        //userScore += 1;
+        userScore += 1;
         output.innerText = "You won the round, Scissor beats Paper!";
     };
-}
+};
 
 btnRock.addEventListener('click', () => {
     userSelection = "rock";
