22a6df9c93c4090f455c418d92ab60213d3558b9
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-21 17:19:31 +0800

    Update app.js
    Stored html elements into variables
    created variables that store user input and computer input
    created a function that generate computer input

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+// HTML elements store in variables
 let output = document.querySelector("#output")
 let btnRock = document.querySelector(".rock")
 let btnPaper = document.querySelector(".paper")
 let btnScissors = document.querySelector(".scissors")
+// Variables for user input and computer input
+let userSelection;
+let compSelection;
+
+//function that generate input for computer;
+function getComputerInput() {
+    let i = Math.floor(Math.random() * 3) + 1;
+        if (i === 1) {
+            return "rock";
+        } else if (i === 2) {
+            return "paper";
+        } else {
+            return "scissor"
+        };
+};
+
+console.log(getComputerInput());
+
+
 
