94a76dec67469d7c03e93c05f675528fe84ff94d
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-21 15:49:16 +0800

    update html styless and javascript script

---
+++ app.js
@@ -0,0 +1,5 @@
+let output = document.querySelector("#output")
+let btnRock = document.querySelector(".rock")
+let btnPaper = document.querySelector(".paper")
+let btnScissors = document.querySelector(".scissors")
+
